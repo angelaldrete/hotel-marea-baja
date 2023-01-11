499725769f413bc390a999a9f9f465f01a07793b
Angel Aldrete <angel.aldrete96@gmail.com>
2023-01-11 15:04:21 -0800

    Updated project for migration

--- src/main.js
+++ src/main.js
@@ -1,34 +1,35 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
-import axios from 'axios'
+import Vue from "vue";
+import App from "../hotelmb/src/App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
 
 axios.interceptors.request.use(function (request) {
-
   if (store.getters.theAccessToken) {
-    request.headers.Authorization = `Bearer ${store.getters.theAccessToken}`
+    request.headers.Authorization = `Bearer ${store.getters.theAccessToken}`;
   }
 
-  return request
-})
+  return request;
+});
 
-axios.interceptors.response.use(function (response) {
-  return response
-}, function (error) {
-  if (error.response.status === 401) {
-    store.dispatch('logout')
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      store.dispatch("logout");
+    }
+    return Promise.reject(error);
   }
-  return Promise.reject(error)
-})
+);
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
